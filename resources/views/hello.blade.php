3505556b1dc3dac5bfa56714de6b8bc548870f17
andridaniel <andridanielrajagukguk@gmail.com>
2023-11-05 22:57:11 +0800

    first commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Diri</title>
+    <!-- Tambahkan tautan ke Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body class="bg-dark">
+    <div class="container">
+        <h3 class="mt-4 mb-5 text-center text-white">HELLO WORLD</h3>
+
+        <div class="col-md-7 mt-4 bg-white rounded mx-auto">
+            <div class="col-md-6 mt-3 pt-5">
+                <p><strong>Nama:</strong> Andri Daniel Rajagukguk</p>
+                <p><strong>Kelas:</strong> FSWD (Full Stack Web Devloper)</p>
+                <p><strong>Alamat:</strong> Kuta, Bali</p>
+                <p><strong>Asal Kampus:</strong> ITB Stikom Bali</p>
+                <p><strong>Email:</strong> gamshot27@gmail.com</p>
+                <p><strong>Nomor Telepon:</strong> 081122334455</p>
+            </div>
+            <div class="col-md-6 ">
+                <p><strong>Hobby</strong></p>
+                <ul>
+                    <li>Sepak bola</li>
+                    <li>Futsal</li>
+                    <li>Badminton</li>
+                    <li>Berenang</li>
+                </ul>
+            </div>
+            <div class="col-md-6 pb-5">
+                <p><strong>Sosial Media</strong></p>
+                <ul>
+                    <button class="btn-primary rounded">Instagram</button>
+                    <button class="btn-warning rounded">Gmail</button>
+                    <button class="btn-dark rounded ">Twitter</button>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <!-- Tambahkan tautan ke Bootstrap JavaScript dan jQuery (opsional) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
