47fdc327d564e7bce14ef31e608d4b08abcc6431
andridaniel <andridanielrajagukguk@gmail.com>
2023-11-21 11:25:55 +0800

    Authentication, middleware, session

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Models\ProductCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Pagination\Paginator;
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,16 +15,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/profile', [HomeController::class, 'profile'])->name('profile');
-Route::get('/products', [HomeController::class, 'products'])->name('products');
-Route::get('/create', [HomeController::class, 'create'])->name('create');
-Route::post('/store', [HomeController::class, 'store'])->name('store');
-Route::get('/edit/{id}', [HomeController::class, 'edit'])->name('edit');
-Route::put('/update/{id}', [HomeController::class, 'update'])->name('update');
-Route::delete('/delete/{id}', [HomeController::class, 'delete'])->name('delete');
-Route::get('/search', [HomeController::class, 'search'])->name('search');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::middleware('auth')->group( function () {
+    Route::get('/dashboard', [HomeController::class, 'index'])->name('home');
+    Route::get('/profile', [HomeController::class, 'profile'])->name('profile');
+    Route::get('/products', [HomeController::class, 'products'])->name('products');
+    Route::get('/create', [HomeController::class, 'create'])->name('create');
+    Route::post('/store', [HomeController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [HomeController::class, 'edit'])->name('edit');
+    Route::put('/update/{id}', [HomeController::class, 'update'])->name('update');
+    Route::delete('/delete/{id}', [HomeController::class, 'delete'])->name('delete');
+    Route::get('/search', [HomeController::class, 'search'])->name('search');
+});
+
+require __DIR__.'/auth.php';
